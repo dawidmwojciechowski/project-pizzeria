24a55afe9bab1069563137970e8ec5b3a4091112
Dawid Wojciechowski <de.wojciechowski@gmail.com>
2021-01-20 10:06:48 +0100

    add correct initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -86,17 +86,17 @@
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.all.menuProduct.clickable);
+      const accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click  */
-      clickableTrigger.addeventListener('click', function(event){
+      accordionTrigger.addEventListener('click', function(event){
 
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(select.all.menuProductsActive);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if(activeProduct !== null && activeProduct != thisProduct.element){
+        if(activeProduct != null && activeProduct != thisProduct.element){
           activeProduct.classList.remove('active');
         }
         /* toggle active class on thisProduct.element */
