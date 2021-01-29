81cf75859f51fb7d679d44f505999559691ee5a2
dawid m. wojciechowski <de.wojciechowski@gmail.com>
2021-01-29 12:49:33 +0100

    module 8.9 finished

--- src/js/script.js
+++ src/js/script.js
@@ -381,6 +381,9 @@
       thisCart.dom.subtotalPrice = element.querySelector(select.cart.subtotalPrice);
       thisCart.dom.totalPrice = element.querySelector(select.cart.totalPrice);
       thisCart.dom.totalNumber = element.querySelector(select.cart.totalNumber);
+      thisCart.dom.form = element.querySelector(select.cart.form);
+      thisCart.dom.address = element.querySelector(select.cart.address);
+      thisCart.dom.phone = element.querySelector(select.cart.phone);
     }
 
     initActions(){
@@ -397,6 +400,11 @@
 
       thisCart.dom.productList.addEventListener('remove', function() {
         thisCart.remove(event.detail.cartProduct);
+      });
+
+      thisCart.dom.form.addEventListener('submit', function(){
+        event.preventDefault ();
+        thisCart.sendOrder ();
       });
     }
 
@@ -447,6 +455,41 @@
       thisCart.dom.totalPrice.innerHTML = thisCart.totalPrice;
       thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
 
+    }
+
+    sendOrder(){
+      const thisCart = this;
+      const url = settings.db.url + '/' + settings.db.order;
+
+      const payload = {
+        address: thisCart.dom.address['value'],
+        phone: thisCart.dom.phone['value'],
+        totalPrice: thisCart.totalPrice,
+        subTotalPrice: thisCart.subtotalPrice,
+        totalNumber: thisCart.totalNumber,
+        deliveryFee: thisCart.deliveryFee,
+        products: [],
+      };
+
+      for(let prod of thisCart.products) {
+        payload.products.push(prod.getData());
+      }
+      console.log('payload', payload);
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      };
+
+      fetch(url, options)
+        .then(function(response) {
+          return response.json();
+        }).then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+        });
     }
   }
 
@@ -530,7 +573,6 @@
       };
 
       return products;
-
     }
   }
 
@@ -550,7 +592,7 @@
       const thisApp = this;
 
       thisApp.data = {};
-      const url = settings.db.url + '/' + settings.db.products;
+      const url = settings.db.url + '/' + settings.db.product;
 
       fetch(url)
         .then(function(rawResponse){
